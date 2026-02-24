1974ff15155d37a7c77218c02cd1a61f4be974a2
Kevin <toidi357@gmail.com>
2026-02-23 21:35:24 -0800

    add https

--- checks/default/https-exists.py
+++ checks/default/https-exists.py
@@ -1,5 +1,4 @@
 import requests
-import json
 from logger import scoring_log as logger
 from termcolor import colored, cprint
 from colorama import init
@@ -7,24 +6,25 @@ init()
 
 def run(host, metadata):
     """
-    Ollama
+    This check requests a given webpage over http port 80, and ensures response code begins with 2 or 3
     
+    Expects metadata: path,
     """
+    path = metadata.get('path', '')
+    
+    if not path:
+        cprint(f"{host} http-exists: - Missing metadata (path)", "red")
+        return False
 
-    url = f'http://{host}/api/generate'
+    url = f'https://{host}/{path}'
 
     try:
-        r = requests.post(url, data=json.dumps({
-            "model": "gemma3:1b",
-            "prompt": "what's 1 + 1",
-            "options": {"num_predict": 20},
-            "stream": False
-        }), timeout=10)
+        r = requests.get(url, timeout=5, verify=False)
 
-        if r.status_code == 200:
+        if r.status_code > 199 and r.status_code < 400: # what a way to check this lol
             return True
         
-        logger.info(f'Check Failed: [{host}] - Ollama failed')
+        logger.info(f"Check Failed: [{host}] - Path returns status code {r.status_code}")
         return False
 
     except requests.exceptions.Timeout as e:
